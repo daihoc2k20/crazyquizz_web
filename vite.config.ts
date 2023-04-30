c56e6b09ba502d3abf2b9f7c68e437d51948ed64
Nguyễn Quốc Đại <20020389@vnu.edu.vn>
2023-05-01 01:51:20 +0700

    setup http

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "vite";
+import { ViteDevServer, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const LoginHtmlFallbackPlugin = {
+  name: "login-html-fallback",
+  configureServer(server: ViteDevServer) {
+    server.middlewares.use("/quizz_web", (req, res, next) => {
+      req.originalUrl += "/";
+      next();
+    });
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), LoginHtmlFallbackPlugin],
   server: {
     port: 2812,
   },
